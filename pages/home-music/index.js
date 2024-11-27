ac28f8aa604725f6dba96631a61790c459e0663c
Xu <whitenight.xiaxu@gmail.com>
2024-11-27 15:50:17 +0800

    swiper image

--- pages/home-music/index.js
+++ pages/home-music/index.js
@@ -1,5 +1,6 @@
 // pages/home-music/index.js
 import { getBanners} from '../../service/api.homemusic'
+import selectRect from  '../../utils/select-rect'
 
 Page({
 
@@ -7,19 +8,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    swpierHeight:0,
+    banners:[]
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-    this.getPageData()
-  },
 
   getPageData(){
     getBanners().then( res => {
-      console.log('res',res)
+     this.setData({banners:res.banners})
     })
   },
 
@@ -31,53 +27,18 @@ Page({
     })
   },
 
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  handleSwierImageLoaded() {
+    selectRect(".swpier-image").then( res => {
+      const rect = res[0]
+      this.setData({swpierHeight:rect.height})
+    })
   },
 
   /**
-   * 生命周期函数--监听页面显示
+   * 生命周期函数--监听页面加载
    */
-  onShow() {
-
+  onLoad(options) {
+    this.getPageData()
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
 })
